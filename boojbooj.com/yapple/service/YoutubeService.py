da50677f1fe9bcd46ab14f0f78426fafb49f2cf2
alvin <alvinabad@users.noreply.github.com>
2009-05-27 02:02:20 +0000

    Updated about page

---
+++ boojbooj.com/yapple/service/YoutubeService.py
@@ -0,0 +1,39 @@
+import simplejson as json
+import urllib2
+from StringIO import StringIO
+
+
+MOST_POPULAR_URL = "http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?alt=json"
+#http://gdata.youtube.com/feeds/api/standardfeeds/most_viewed
+
+class YoutubeService:
+    """
+    Youtube API Service
+    """
+    
+    def getMostPopularVideos(self):
+        url = MOST_POPULAR_URL
+        
+        try:
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)    
+            io = StringIO(response.read())
+            self.most_popular_list = json.load(io)
+        except Exception, e:
+            print e
+            return []
+        
+        return self.most_popular_list
+        
+    
+if __name__ == '__main__':
+    ys = YoutubeService()
+    mp = ys.getMostPopularVideos()
+    #print mp['feed']
+    #feed = mp['feed']
+    #print list
+    #print feed['link']
+    #print mp.keys()
+    #for key in mp['feed'].keys():
+    #    print key
+    print mp['feed']['link']    
